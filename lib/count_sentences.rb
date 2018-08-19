6ae6656267114a2611c316bb668de5563f817cbf
Katrina Pritchard <pritckat@gmail.com>
2018-08-19 17:28:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
+
   end
 
   def question?
+    self.end_with?("?") ? true : false
 
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+    array = self.split(/[.?!]+/)
+    array.count
 
   end
 end
